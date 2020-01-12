b7f359b82c722c36b2d0048e808c0969572e4e95
Shunsuke <shun.sskd05@icloud.com>
2020-01-12 06:17:37 +0000

    リレーション修正,book_show-page修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,9 +10,10 @@ class BooksController < ApplicationController
   end
 
   def create
-  	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	if @book.save #入力されたデータをdbに保存する。
-  		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
+  	book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
+    book.user_id = current_user.id
+  	if book.save #入力されたデータをdbに保存する。
+  		redirect_to book_path(book), notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
   		@books = Book.all
   		render 'index'
